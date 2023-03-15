43252e92836cd9f595bf08ae123ca688f00af901
sercanyesilkoy <119282087+sercanyesilkoy@users.noreply.github.com>
2023-03-15 16:18:27 +0900

    More Info In ENG and KR Version
    Also you depens on your input, you can find out the info about me in English or Korean, If you input wrong numbers, I might be angry

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-trial
+name = "YESILKOY SERCAN "
+number = "202103933 "
+major = "Computer Engineering "
+mother_land = "Turkiye"
 
-name = "YESILKOY SERCAN"
-number = "202103933"
-major = "Computer Engineering
+성함 = "예실코이 세르잔 "
+학번 = "202103933 "
+전공 = "컴퓨터공학전공 "
+본국 = "튀르키예"
+
+a = int(input("Write 0 for English, 한국어 안내를 위하여 1 작성해십시오"))
+
+if a == 0:
+              print(name + number + major + mother_land)
+elif a == 1:
+              print(성함 + 학번 + 전공 + 본국)
+else:
+  print("개꿈")
