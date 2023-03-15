4c457d3899d776c2e52c33b52ed9dc471b654ec2
sercanyesilkoy <119282087+sercanyesilkoy@users.noreply.github.com>
2023-03-15 16:36:51 +0900

    Added Age and Fav Excersize
    check it in code

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 name = "YESILKOY SERCAN "
 number = "202103933 "
 major = "Computer Engineering "
-mother_land = "Turkiye"
+mother_land = "Turkiye "
+age = "Sorry it is not public info, you should ask me in person"
 
 성함 = "예실코이 세르잔 "
 학번 = "202103933 "
 전공 = "컴퓨터공학전공 "
-본국 = "튀르키예"
-
+본국 = "튀르키예 "
+나이 = "최송합니다만, 개인 정보이라 여기서 알려드리기 어렵습나다. 실제로 물어보세요."
 a = int(input("Write 0 for English, 한국어 안내를 위하여 1 작성해십시오"))
 
 if a == 0:
-              print(name + number + major + mother_land)
+              print(name + number + major + mother_land + age)
+              print(" I think my favourite exersize is hiking?" )
+    
 elif a == 1:
-              print(성함 + 학번 + 전공 + 본국)
+              print(성함 + 학번 + 전공 + 본국 + 나이)
+              print(" 가장 좋아하는 운동은 등산인 것 같아요?" )
+
 else:
   print("개꿈")
